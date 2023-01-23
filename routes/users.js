bdfe052759c9fd2ad99e65c859bc460b1687cb0d
franco211 <francisbyaruhanga88@gmail.com>
2023-01-23 01:50:14 -0500

    git commit

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,25 @@ router.use(logger);
 
 //router instance of the application
 router.get("/", (req, res) => {
+  console.log(req.query.name);
   res.send("user List");
 });
 
 router.get("/new", (req, res) => {
   //   res.send("User New Form");
-  res.render("users/new", { firstName: "Test" });
+  res.render("users/new");
 });
 
 router.post("/", (req, res) => {
   //   res.send("Create User");
+  const isValid = true;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("Error");
+    res.render("users/new", { firstName: req.body.firstName });
+  }
   console.log(req.body.firstName);
   res.send("hi");
 });
@@ -22,7 +31,7 @@ router.post("/", (req, res) => {
 router
   .route("/:id")
   .get((req, res) => {
-    // console.log(req.user);
+    console.log(req.user);
     res.send(`Get User with ID ${req.params.id}`);
   })
   .put((req, res) => {
@@ -37,4 +46,11 @@ router.param("id", (req, res, next, id) => {
   req.user = users[id];
   next();
 });
+
+//middle wear function.
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
+
 module.exports = router;
