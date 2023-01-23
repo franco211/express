60fd66f03e074f0ee6b3eea859c9aae5ad96a2c4
franco211 <francisbyaruhanga88@gmail.com>
2023-01-23 00:18:09 -0500

    git commit

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,26 @@ router.get("/new", (req, res) => {
   res.send("User New Form");
 });
 
+router.post("/", (req, res) => {
+  res.send("create User");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user);
+    res.send(`Get User with ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update User with ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete User with ID ${req.params.id}`);
+  });
+
+const users = [{ name: "kyle" }, { name: "Sally" }];
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
 module.exports = router;
