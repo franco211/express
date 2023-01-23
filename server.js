7f22b844bec4f2fb13724e7e700ef46d37cccf98
franco211 <francisbyaruhanga88@gmail.com>
2023-01-23 00:29:37 -0500

    git commit

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express"); //create server
 const app = express(); //set up server
 
-app.set("view engine", "ejs"); //view engine
-app.use(logger);
+//app.set("view engine", "ejs"); //view engine
+//app.use(logger);
 
-app.get("/", (req, res) => {
-  console.log("Here"); //run code to access url
-  //res.sendStatus(500).json({ Message: "Error" }); //status with a message
-  // res.send("hi") //send message to the user
-  // res.download("server.js"); //send file to download
-  res.render("index", { text: "World" }); //rendering a file
-});
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   console.log("Here"); //run code to access url
+//   //res.sendStatus(500).json({ Message: "Error" }); //status with a message
+//   // res.send("hi") //send message to the user
+//   // res.download("server.js"); //send file to download
+//   res.render("index", { text: "World" }); //rendering a file
+// });
 
 const userRouter = require("./routes/users");
 
