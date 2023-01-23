df8f419e56bec50af786045b4ae4a5b18aa89c31
franco211 <francisbyaruhanga88@gmail.com>
2023-01-22 22:47:32 -0500

    git commit

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
   console.log("Here"); //run code to access url
   res.sendStatus(500).json({ Message: "Error" }); //status with a message
   // res.send("hi") //send message to the user
+  // res.download("server.js"); //send file to download
+  res.render("index"); //rendering a file
 });
 
 app.listen(3000); //server run
